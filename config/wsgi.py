1ae70a29e6e187673ce75d4188617f8ea3d1b2bb
Mirjalol Qosimov <mirjalol02519@gmail.com>
2026-06-17 00:56:59 +0500

    config/wsgi.py updated for bot start

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,7 +1,19 @@
 import os
-
+import threading
+import subprocess
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+def run_bot():
+    try:
+        subprocess.Popen(["python", "bot/main.py"])
+        print("--- Bot Django ichida parallel fonda ishga tushirildi ---")
+    except Exception as e:
+        print(f"Botni fonda yoqishda xatolik: {e}")
+
+if os.getenv('RAILWAY_ENVIRONMENT') is not None:
+    bot_thread = threading.Thread(target=run_bot, daemon=True)
+    bot_thread.start()
